fdb043b03a22870593ce7dcf76848f324d909af6
Anna <aniuta1803@gmail.com>
2023-06-23 19:54:25 -0500

    Anna

--- src/main/java/classaaa.java
+++ src/main/java/classaaa.java
@@ -1,6 +1,10 @@
 public class classaaa {
     public static void main(String[] args) {
         System.out.println("New Branch");
+
         System.out.println("second branch");
+
+        System.out.println("this change is from master");
+
     }
 }
